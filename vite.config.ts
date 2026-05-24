f8b28c376a3e8ed743cc6889de2a360298ae50ab
Rafli Alif Ihza Hartono <64510812+rhaffle87@users.noreply.github.com>
2026-05-24 21:15:32 +0700

    chore: replace custom Vercel preset with nitro plugin and update gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: {
-        preset: "vercel",
-      },
-    } as any),
+    tanstackStart(),
+    nitro(),
     tsconfigPaths(),
     react(),
     tailwindcss(),
