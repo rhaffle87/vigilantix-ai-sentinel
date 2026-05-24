fb5c09345e1d75e042878b71d44c6edce8e07fd4
Rafli Alif Ihza Hartono <64510812+rhaffle87@users.noreply.github.com>
2026-05-24 21:11:39 +0700

    chore: configure tanstackStart to use vercel preset in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart(),
+    tanstackStart({
+      server: {
+        preset: "vercel",
+      },
+    } as any),
     tsconfigPaths(),
     react(),
     tailwindcss(),
